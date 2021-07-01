04e44b3947b4f91f54ea0d743bf5f3b932e0a11e
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-07-01 17:45:40 +0430

    add title for cards

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -6,7 +6,8 @@ import {MDBRow} from "mdb-react-ui-kit";
 function Cards() {
     return (
         <div className="container mt-5">
-            <MDBRow className='row-cols-1 row-cols-md-4 g-4'>
+            <h2 className="text-center">جدید ترین محصولات سهند تابلو</h2>
+            <MDBRow className='row-cols-1 row-cols-md-4 g-4 mt-5'>
                 <Card />
                 <Card />
                 <Card />
