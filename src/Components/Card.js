56ac7209dc42c39b10e7b17a70fd793df23daa9e
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-07-01 17:15:41 +0430

    edit Card as a component and add Cards component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import { MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBCol } from 'mdb-react-ui-kit';
 
 
 function Card() {
     return (
-        <div className="container mt-5">
-            <MDBRow className='row-cols-1 row-cols-md-4 g-4'>
-                <MDBCol>
-                    <MDBCard className='h-100'>
+        <MDBCol>
+            <MDBCard className='h-100'>
                         <MDBCardImage
                             src='https://mdbcdn.b-cdn.net/img/new/standard/city/041.jpg'
                             alt='...'
@@ -21,54 +19,54 @@ function Card() {
                             </MDBCardText>
                         </MDBCardBody>
                     </MDBCard>
-                </MDBCol>
-                <MDBCol>
-                    <MDBCard className='h-100'>
-                        <MDBCardImage
-                            src='https://mdbcdn.b-cdn.net/img/new/standard/city/042.jpg'
-                            alt='...'
-                            position='top'
-                        />
-                        <MDBCardBody>
-                            <MDBCardTitle>Card title</MDBCardTitle>
-                            <MDBCardText>This is a short card.</MDBCardText>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-                <MDBCol>
-                    <MDBCard className='h-100'>
-                        <MDBCardImage
-                            src='https://mdbcdn.b-cdn.net/img/new/standard/city/043.jpg'
-                            alt='...'
-                            position='top'
-                        />
-                        <MDBCardBody>
-                            <MDBCardTitle>Card title</MDBCardTitle>
-                            <MDBCardText>
-                                This is a longer card with supporting text below as a natural lead-in to additional content.
-                            </MDBCardText>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-                <MDBCol>
-                    <MDBCard className='h-100'>
-                        <MDBCardImage
-                            src='https://mdbcdn.b-cdn.net/img/new/standard/city/044.jpg'
-                            alt='...'
-                            position='top'
-                        />
-                        <MDBCardBody>
-                            <MDBCardTitle>Card title</MDBCardTitle>
-                            <MDBCardText>
-                                This is a longer card with supporting text below as a natural lead-in to additional content.
-                                This content is a little bit longer.
-                            </MDBCardText>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-            </MDBRow>
-        </div>
+        </MDBCol>
     )
 }
 
 export default Card;
+
+
+// <MDBCol>
+//     <MDBCard className='h-100'>
+//         <MDBCardImage
+//             src='https://mdbcdn.b-cdn.net/img/new/standard/city/042.jpg'
+//             alt='...'
+//             position='top'
+//         />
+//         <MDBCardBody>
+//             <MDBCardTitle>Card title</MDBCardTitle>
+//             <MDBCardText>This is a short card.</MDBCardText>
+//         </MDBCardBody>
+//     </MDBCard>
+// </MDBCol>
+// <MDBCol>
+//     <MDBCard className='h-100'>
+//         <MDBCardImage
+//             src='https://mdbcdn.b-cdn.net/img/new/standard/city/043.jpg'
+//             alt='...'
+//             position='top'
+//         />
+//         <MDBCardBody>
+//             <MDBCardTitle>Card title</MDBCardTitle>
+//             <MDBCardText>
+//                 This is a longer card with supporting text below as a natural lead-in to additional content.
+//             </MDBCardText>
+//         </MDBCardBody>
+//     </MDBCard>
+// </MDBCol>
+// <MDBCol>
+//     <MDBCard className='h-100'>
+//         <MDBCardImage
+//             src='https://mdbcdn.b-cdn.net/img/new/standard/city/044.jpg'
+//             alt='...'
+//             position='top'
+//         />
+//         <MDBCardBody>
+//             <MDBCardTitle>Card title</MDBCardTitle>
+//             <MDBCardText>
+//                 This is a longer card with supporting text below as a natural lead-in to additional content.
+//                 This content is a little bit longer.
+//             </MDBCardText>
+//         </MDBCardBody>
+//     </MDBCard>
+// </MDBCol>
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Card from "./Card";
+import {MDBRow} from "mdb-react-ui-kit";
 
 
 function Cards() {
     return (
-        <>
-            <Card />
-        </>
+        <div className="container mt-5">
+            <MDBRow className='row-cols-1 row-cols-md-4 g-4'>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </MDBRow>
+        </div>
     );
 }
 
